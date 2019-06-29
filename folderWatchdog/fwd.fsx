3c92d1ee7db3126a0c16e88d48c2e05cb9d4e13f
darel <darelgrif@gmail.com>
2019-06-29 12:03:01 -0700

    initial commit of project files

---
+++ folderWatchdog/fwd.fsx
@@ -0,0 +1,17 @@
+﻿open System.IO
+let EnumFiles path exts = Directory.GetFiles(path, exts, SearchOption.AllDirectories)
+let ByteToMB szB = szB * 0.000001
+let files = EnumFiles "C:\\sr\\" "*"
+
+let mutable totalSize = int64 0
+let updateFSize a b = a + b
+//let totalSize =
+for eachFile in files do
+    let fInfo = FileInfo(eachFile)
+    let bFSize = fInfo.Length
+    let strFSize = fInfo.Length.ToString()
+    printfn "[+]FILE: %s\n[--]SIZE: %s bytes" eachFile strFSize
+    totalSize <- updateFSize totalSize bFSize
+
+let szInMb = ByteToMB ((float)totalSize)
+printfn "Total size: %i MB" (int64(szInMb))
