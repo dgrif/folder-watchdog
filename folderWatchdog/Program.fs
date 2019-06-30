ae565cac0bbc632ea816dd2d35c9f881e3463d29
darel <darelgrif@gmail.com>
2019-06-30 11:06:18 -0700

    some minor updates and additional functionality.

--- folderWatchdog/Program.fs
+++ folderWatchdog/Program.fs
@@ -1,6 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System.IO
+let EnumFiles path exts = Directory.GetFiles(path, exts, SearchOption.AllDirectories)
+let ByteToMB szB = szB * 0.000001
+let updateFSize a b = a + b
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
--- folderWatchdog/fwd.fsx
+++ folderWatchdog/fwd.fsx
@@ -1,17 +1,32 @@
-﻿open System.IO
+﻿open System
+open System.IO
 let EnumFiles path exts = Directory.GetFiles(path, exts, SearchOption.AllDirectories)
+
+let DeleteFiles files =
+    for eachFile in files do
+        //printfn "Deleting: %s" eachFile
+        File.Delete(eachFile)
+        
+
 let ByteToMB szB = szB * 0.000001
-let files = EnumFiles "C:\\sr\\" "*"
-
-let mutable totalSize = int64 0
+//let MBtoByte szM = szM / 0.000001 //TODO: verify this, it's prob incorrect
 let updateFSize a b = a + b
-//let totalSize =
-for eachFile in files do
-    let fInfo = FileInfo(eachFile)
-    let bFSize = fInfo.Length
-    let strFSize = fInfo.Length.ToString()
-    printfn "[+]FILE: %s\n[--]SIZE: %s bytes" eachFile strFSize
-    totalSize <- updateFSize totalSize bFSize
 
-let szInMb = ByteToMB ((float)totalSize)
-printfn "Total size: %i MB" (int64(szInMb))
+//let files = EnumFiles "C:\\sr\\" "*"
+
+let watchFolder folder maxSize timerInterval = 
+//let totalSize =
+    let files = EnumFiles folder "*"
+    let mutable totalSize = int64 0
+    let timer = new System.Timers.Timer(float timerInterval)
+
+    for eachFile in files do
+        let fInfo = FileInfo(eachFile)
+        let bFSize = fInfo.Length
+        let strFSize = fInfo.Length.ToString()
+        printfn "[+]FILE: %s\n[--]SIZE: %s bytes" eachFile strFSize
+        totalSize <- updateFSize totalSize bFSize
+
+    let szInMb = ByteToMB ((float)totalSize)
+    printfn "Total size: %i MB" (int64(szInMb))
+    totalSize
