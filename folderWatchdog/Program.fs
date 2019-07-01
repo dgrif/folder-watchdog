2be9b297df32d93a3c9db99ce40ec33c84163089
darel <darelgrif@gmail.com>
2019-06-30 22:50:23 -0700

    updates for main, and debug program args

--- folderWatchdog/Program.fs
+++ folderWatchdog/Program.fs
@@ -2,10 +2,22 @@
 // See the 'F# Tutorial' project for more help.
 open System
 open System.IO
-let EnumFiles path exts = Directory.GetFiles(path, exts, SearchOption.AllDirectories)
-let ByteToMB szB = szB * 0.000001
-let updateFSize a b = a + b
+
+let EnumFiles path exts = 
+    Directory.GetFiles(path, exts, SearchOption.AllDirectories)
+
+let ByteToMB szB = 
+    szB * 0.000001
+
+let updateFSize a b = 
+    a + b
+
 //CalcFolderSize - calculates the size of all files in a folder, returned in bytes
+let DeleteFiles (files : string[]) =
+    for eachFile in files do
+        //printfn "Deleting: %s" eachFile
+        File.Delete(eachFile)
+
 let CalcFolderSize folder = 
 //let totalSize =
     let files = EnumFiles folder "*"
@@ -20,7 +32,35 @@ let CalcFolderSize folder =
 
     totalSize
 
+let FolderWatchdogHandler folderToCheck maxSize = 
+    let totalSize = CalcFolderSize folderToCheck
+    printfn "[+] Folder Size: %iMB" (int64(ByteToMB ((float)totalSize)))
+    let szInMb = ByteToMB ((float)totalSize)
+    let files = EnumFiles folderToCheck "*"
+    if szInMb >= maxSize then
+        printfn "\t[!] Total size exceeds threashold: %i MB\nDeleting files!" (int64(szInMb))
+        DeleteFiles files
+
+let WatchdogTimer timerInterval wdh =
+    let timer = new Timers.Timer(float timerInterval)
+    
+    timer.AutoReset <- true
+    timer.Elapsed.Add wdh
+    async {
+        timer.Start()
+        do! Async.Sleep timerInterval
+        //DEBUG, in the program this will be infinite until program dies.
+        //timer.Stop()
+    }
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let numArgs = argv.Length
+    if numArgs = 2 then
+        printfn "arg1: %s arg2: %s" argv.[0] argv.[1]
+        let wdHandler _ = FolderWatchdogHandler argv.[0] (argv.[1] |> float)
+        let aTimer = WatchdogTimer (argv.[3] |> int) wdHandler
+        Async.RunSynchronously aTimer
+        0
+    else
+        1 // return an integer exit code
