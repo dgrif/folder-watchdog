cd7af51046552111ea65dd09a738837d3cdcbb9a
darel <darelgrif@gmail.com>
2019-06-30 17:02:14 -0700

    function updates, messing with async and timers.

--- folderWatchdog/Program.fs
+++ folderWatchdog/Program.fs
@@ -1,10 +1,24 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
-
+open System
 open System.IO
 let EnumFiles path exts = Directory.GetFiles(path, exts, SearchOption.AllDirectories)
 let ByteToMB szB = szB * 0.000001
 let updateFSize a b = a + b
+//CalcFolderSize - calculates the size of all files in a folder, returned in bytes
+let CalcFolderSize folder = 
+//let totalSize =
+    let files = EnumFiles folder "*"
+    let mutable totalSize = int64 0
+    
+    for eachFile in files do
+        let fInfo = FileInfo(eachFile)
+        let bFSize = fInfo.Length
+        let strFSize = fInfo.Length.ToString()
+        printfn "[+]FILE: %s\n[--]SIZE: %s bytes" eachFile strFSize
+        totalSize <- updateFSize totalSize bFSize
+
+    totalSize
 
 [<EntryPoint>]
 let main argv = 
--- folderWatchdog/fwd.fsx
+++ folderWatchdog/fwd.fsx
@@ -1,8 +1,10 @@
 ﻿open System
 open System.IO
+open System.Threading
+
 let EnumFiles path exts = Directory.GetFiles(path, exts, SearchOption.AllDirectories)
 
-let DeleteFiles files =
+let DeleteFiles (files : string[]) =
     for eachFile in files do
         //printfn "Deleting: %s" eachFile
         File.Delete(eachFile)
@@ -14,19 +16,41 @@ let updateFSize a b = a + b
 
 //let files = EnumFiles "C:\\sr\\" "*"
 
-let watchFolder folder maxSize timerInterval = 
+//CalcFolderSize - calculates the size of all files in a folder, returned in bytes
+let CalcFolderSize folder = 
 //let totalSize =
     let files = EnumFiles folder "*"
     let mutable totalSize = int64 0
-    let timer = new System.Timers.Timer(float timerInterval)
-
+    
     for eachFile in files do
         let fInfo = FileInfo(eachFile)
         let bFSize = fInfo.Length
         let strFSize = fInfo.Length.ToString()
         printfn "[+]FILE: %s\n[--]SIZE: %s bytes" eachFile strFSize
         totalSize <- updateFSize totalSize bFSize
-
-    let szInMb = ByteToMB ((float)totalSize)
-    printfn "Total size: %i MB" (int64(szInMb))
     totalSize
+
+let FolderWatchdogHandler folderToCheck maxSize = 
+    let totalSize = CalcFolderSize folderToCheck
+    printfn "[ ] Folder Size: %i" (int64(ByteToMB ((float)totalSize)))
+    let szInMb = ByteToMB ((float)totalSize)
+    let files = EnumFiles folderToCheck "*"
+    if szInMb >= maxSize then
+        printfn "\t[!] Total size exceeds threashold: %i MB\nDeleting files!" (int64(szInMb))
+        DeleteFiles files
+
+let WatchdogTimer timerInterval wdh =
+    let timer = new Timers.Timer(float timerInterval)
+    
+    timer.AutoReset <- true
+    timer.Elapsed.Add wdh
+    async {
+        timer.Start()
+        do! Async.Sleep timerInterval
+    }
+
+// folder and time should be variables, input as cli args or prompted for if none
+let wdHandler _ = FolderWatchdogHandler "C:\\temp" 100000.0
+let aTimer = WatchdogTimer 30000 wdHandler
+Async.RunSynchronously aTimer
+//let wdt = WatchdogTimer 30 (FolderWatchdogHandler "C:\\temp" 100000.0)
